8a01b04cc036184261efb0349b91840852dfa631
ArturHideg <arturhideg@gmail.com>
2022-01-21 17:31:55 +0100

    new small boat and thinking about wawe controller

---
+++ TreasureDefence/Assets/__Scrips/WaweController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaweController : MonoBehaviour
+{
+    public float gameTime;
+    public float waweTime; //one wawe lasts the time
+    public int currentWawe; 
+    public int vaweForLevel; //each map has more wawes
+    
+    public GameObject winScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waweTime > gameTime)
+        {
+
+        }
+
+        if (currentWawe < vaweForLevel)
+        {
+            winScreen.SetActive(true);
+        }
+    }
+}
