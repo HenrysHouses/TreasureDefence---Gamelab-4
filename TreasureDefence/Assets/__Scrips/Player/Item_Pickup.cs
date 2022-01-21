0a5e6a5980b857f0fac4d15c3f32845e4a313bad
Mikkel R.Tvedt <mikkelking97@gmail.com>
2022-01-21 11:55:59 +0100

    StartPickUp Script.

---
+++ TreasureDefence/Assets/__Scrips/Player/Item_Pickup.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Pickup : MonoBehaviour
+{
+    public float pickUpRange = 5f;
+
+    public bool canPickUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
