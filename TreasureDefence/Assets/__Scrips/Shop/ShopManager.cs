9495ce2ee65da26a390d8173f49f696a9d6c4a50
Mikkel R.Tvedt <mikkelking97@gmail.com>
2022-01-28 17:01:14 +0100

    ShopManager.

--- TreasureDefence/Assets/__Scrips/Shop/ShopManager.cs
+++ TreasureDefence/Assets/__Scrips/Shop/ShopManager.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopManager : MonoBehaviour
-{
-    public GameObject StandardTower;
-    public Transform SpawnLocation;
+{ public int total;
+    Tower_Interactable ti;
 
-    public void SpawnTowerAtPoint()
+    public List<TowerInfo> items = new List<TowerInfo>();  
+
+    private void OnTriggerEnter(Collider other)
     {
-        Instantiate(StandardTower, SpawnLocation.position, Quaternion.identity);
-        Cursor.lockState = CursorLockMode.Locked;
+        ti = other.gameObject.GetComponent<Tower_Interactable>().towerInfo;
+
+        if (ti != null)
+        {
+            items.Add(ti.TowerInfo); 
+        }
+        
     }
 
-  
+    private void OnTriggerExit(Collider other)
+    {
+        items.Remove(other.gameObject.GetComponent<Tower_Interactable>().towerInfo);
+    
+
+    }
+
+
+
+    public void PayForTower()
+    {
+        for(int i = 0; i < items.Count; i++)
+        {
+            total += items[i].cost;
+        }
+
+       if(GameManager.instance.money >= total)
+        {
+            GameManager.instance.money -= total;
+            Debug.Log("You're rich.");
+            
+        }
+       else
+        {
+            Debug.Log("You're poor.");
+        }
+        total = 0;
+    }
+
 }
