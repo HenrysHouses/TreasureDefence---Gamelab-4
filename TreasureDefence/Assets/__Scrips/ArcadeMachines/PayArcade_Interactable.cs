312fb29bb4c8655af2e44197d07a4798b873fae6
roock89 <77836346+roock89@users.noreply.github.com>
2022-02-10 02:09:15 +0100

    Update PayArcade_Interactable.cs

--- TreasureDefence/Assets/__Scrips/ArcadeMachines/PayArcade_Interactable.cs
+++ TreasureDefence/Assets/__Scrips/ArcadeMachines/PayArcade_Interactable.cs
@@ -8,7 +8,7 @@ public class PayArcade_Interactable : Interactable
 
     public override void InteractTrigger(object target = null)
     {
-        if(CurrencyManager.instance.money > arcade.ArcadeCost && !arcade.isPlaying)
+        if(CurrencyManager.instance.money >= arcade.ArcadeCost && !arcade.isPlaying)
         {
             CurrencyManager.instance.SubtractMoney(arcade.ArcadeCost);
             arcade.StartGame();
