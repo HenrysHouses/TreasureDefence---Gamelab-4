b06accb1307738d782cda3959c9514e87a1e2e9f
Mikkel R.Tvedt <mikkelking97@gmail.com>
2022-02-15 14:37:36 +0100

    ShootingRange Minor update.

--- TreasureDefence/Assets/__Scrips/ArcadeMachines/ShootingRange/ShootingRange.cs
+++ TreasureDefence/Assets/__Scrips/ArcadeMachines/ShootingRange/ShootingRange.cs
@@ -11,6 +11,9 @@ public class ShootingRange : ArcadeMachine
 
     [Tooltip("A bool to activate the targets left and right movement.")]
     public bool TargetMovement;
+    [Tooltip("The Speed of the Target")]
+    public int TargetMovementSpeed;
+    public float num;
 
     private GameObject TargetPos1, TargetPos2;
     // Base Arcade Behaviour
@@ -18,32 +21,37 @@ public class ShootingRange : ArcadeMachine
     //Start the Game. What it costs.
     public override void StartSetup()
     {  
-        TargetMovement = true;
-
         base.StartSetup();
         
-        Gun.transform.position = GunHoldPosition.transform.position;
         Debug.Log("This is what happens at start");
     }
 
     public void OnTriggerEnter(Collider other)
     {   
-            StartSetup();
+        if(other.gameObject.tag == "Player")
+        {
+            StartGame();
+            Debug.Log("Works?");
+            TargetMovement = true;
+        }
     }
 
     public override void isPlayingUpdate()
     {
-        if (TargetMovement = true)
+        num += TargetMovementSpeed * Time.deltaTime;
+        Vector3 pos = Target.transform.position;
+        pos.z = PingPongExtention(num, 0, 1) + -5;
+        Target.transform.position = pos;
+
+            Gun.transform.position = GunHoldPosition.transform.position;
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
         {
-            Target.transform.position = TargetPos1.transform.position;
 
-          /*  if()
-            {
-                Target.transform.position = TargetPos2.transform.position;
-            }     */
         }
-
-        
 
         return ;
     }
@@ -68,6 +76,20 @@ public class ShootingRange : ArcadeMachine
 
     }
     
+    public float PingPongExtention(float t, float rangeFrom, float rangeTo)
+    {
+        float remapped = 0;
+        if (t % rangeTo > rangeTo/2)
+        {
+            remapped = ExtensionMethods.Remap(t % rangeTo, rangeFrom, rangeTo, rangeTo, rangeFrom);
+        }
+        else
+        {
+            remapped = t % rangeTo;
+        }
+            return remapped;
+
+    }
 
 
     //While Playing.
