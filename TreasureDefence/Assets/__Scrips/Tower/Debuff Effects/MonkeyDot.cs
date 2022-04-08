be19812498b180d119c044b6dfacc0db6c3a4584
roock89 <77836346+roock89@users.noreply.github.com>
2022-04-08 18:00:28 +0200

    added monkey catapult to the vending machine

--- TreasureDefence/Assets/__Scrips/Tower/Debuff Effects/MonkeyDot.cs
+++ TreasureDefence/Assets/__Scrips/Tower/Debuff Effects/MonkeyDot.cs
@@ -30,6 +30,5 @@ public class MonkeyDot : EnemyDebuff
     public override void ExpirationTrigger()
     {
         GameObject.Destroy(monkeyObject.gameObject);
-        Debug.Log("destroyed");
     }
 }
