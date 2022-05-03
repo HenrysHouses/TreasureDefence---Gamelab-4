3c893de85bf10c0541296eefcaba35beb0c47cee
roock89 <77836346+roock89@users.noreply.github.com>
2022-05-03 13:25:57 +0200

    just some stuff

--- TreasureDefence/Assets/__Scrips/SceneManagement/SceneChanger.cs
+++ TreasureDefence/Assets/__Scrips/SceneManagement/SceneChanger.cs
@@ -3,7 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+
     public void loadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+       public void loadScene(string scene)
     {
         SceneManager.LoadScene(scene);
     }
