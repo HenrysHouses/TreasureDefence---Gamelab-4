6f74e3062892f9eac0b585d1098ab51aef73a2ea
Rune <runeandreasnohr@gmail.com>
2022-05-26 21:52:06 +0200

    Pirate Cove Working

--- TreasureDefence/Assets/__Scrips/Managers/PirateCoveController.cs
+++ TreasureDefence/Assets/__Scrips/Managers/PirateCoveController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PirateCoveController : MonoBehaviour
 {
@@ -9,6 +10,19 @@ public class PirateCoveController : MonoBehaviour
     
     private int _experience;
 
+    public TextMeshPro experienceText;
+
+    public TextMeshPro[] costs;
+
+    public GameObject[] checkSigns;
+    public GameObject[] crossSigns;
+
+    public GameObject[] upgradeIcons;
+    
+    private float[] mult = { 1f, 1.08f, 1.32f, 1.25f, 1.42f };
+
+    private int[] buildingsLevels = {0,0,0,0,0};
+    
     /*                                                                      Descriptions
      ________________________________________________________________________________________________________________________________ 
     |                                                                                                                               |
@@ -60,7 +74,7 @@ public class PirateCoveController : MonoBehaviour
     [SerializeField] private GameObject[] tavernModels;
     [SerializeField] private GameObject[] blacksmithModels;
     [SerializeField] private GameObject[] barracksModels;
-    [SerializeField] private GameObject[] guardTowerModels;
+    //[SerializeField] private GameObject[] guardTowerModels;
 
     private float[] wallUpgrades = { 1f, 1.25f, 1.5f,  1.9f, 2.4f, 3f };
 
@@ -72,12 +86,14 @@ public class PirateCoveController : MonoBehaviour
 
 
     // Upgrades
+    /*
     private int _wallLevel;
     private int _chestLevel;
     private int _tavernLevel;
     private int _blacksmithLevel;
     private int _barracksLevel;
-    private int _guardTowerLevel;
+    */
+    //private int _guardTowerLevel;
 
     void Start()
     {
@@ -89,59 +105,191 @@ public class PirateCoveController : MonoBehaviour
         {
             Destroy(this);
         }
-        
-        for (int i = 0; i < 10; i++)
-        {
-            int temp = i + 1;
 
+<<<<<<< Updated upstream
             // Debug.Log("Level " + temp + " : " + CalculateCost(i) + " xp.");
+=======
+        for (int i = 0; i < costs.Length; i++)
+        {
+            costs[i].text = "" + CalculateCost(0, mult[i]);
+            checkSigns[i].SetActive(false);
+>>>>>>> Stashed changes
         }
+
+        experienceText.text = "" + _experience;
+
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Application.isEditor)
         {
-            AddExperience(1);
-            Debug.Log("Experience: " + _experience);
-        }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                AddExperience(1);
+                Debug.Log("Experience: " + _experience);
+            }
         
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Upgrade(0);
-        }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Upgrade(0);
+            }
         
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            Upgrade(1);
-        }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Upgrade(1);
+            }
         
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            Upgrade(2);
-        }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Upgrade(2);
+            }
         
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            Upgrade(3);
-        }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                Upgrade(3);
+            }
         
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            Upgrade(4);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            Upgrade(5);
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                Upgrade(4);
+            }
         }
     }
 
-    public int CalculateCost(int structureLevel)
+    public void CalculateAll()
+    {
+        /*
+        for (int i = 0; i < checkSigns.Length; i++)
+        {
+            if (_experience > CalculateCost(i))
+            {
+                checkSigns[i].SetActive(true);
+                crossSigns[i].SetActive(false);
+            }
+            else
+            {
+                checkSigns[i].SetActive(false);
+                crossSigns[i].SetActive(true);
+            }
+        }
+        */
+
+        for (int i = 0; i < buildingsLevels.Length; i++)
+        {
+            if (_experience >= CalculateCost(buildingsLevels[i], mult[i]))
+            {
+                upgradeIcons[i].SetActive(true);
+                checkSigns[i].SetActive(true);
+                crossSigns[i].SetActive(false);
+            }
+            else
+            {
+                upgradeIcons[i].SetActive(false);
+                checkSigns[i].SetActive(false);
+                crossSigns[i].SetActive(true);
+            }
+        }
+        
+        /*
+        if (_experience >= CalculateCost(_wallLevel, mult[0]))
+        {
+            upgradeIcons[0].SetActive(true);
+            checkSigns[0].SetActive(true);
+            crossSigns[0].SetActive(false);
+        }
+        else
+        {
+            upgradeIcons[0].SetActive(false);
+            checkSigns[0].SetActive(false);
+            crossSigns[0].SetActive(true);
+        }
+        
+        if (_experience >= CalculateCost(_chestLevel, mult[1]))
+        {
+            upgradeIcons[1].SetActive(true);
+            checkSigns[1].SetActive(true);
+            crossSigns[1].SetActive(false);
+        }
+        else
+        {
+            upgradeIcons[1].SetActive(false);
+            checkSigns[1].SetActive(false);
+            crossSigns[1].SetActive(true);
+        }
+        
+        if (_experience >= CalculateCost(_tavernLevel, mult[2]))
+        {
+            upgradeIcons[2].SetActive(true);
+            checkSigns[2].SetActive(true);
+            crossSigns[2].SetActive(false);
+        }
+        else
+        {
+            upgradeIcons[2].SetActive(false);
+            checkSigns[2].SetActive(false);
+            crossSigns[2].SetActive(true);
+        }
+        
+        if (_experience >= CalculateCost(_blacksmithLevel, mult[3]))
+        {
+            upgradeIcons[3].SetActive(true);
+            checkSigns[3].SetActive(true);
+            crossSigns[3].SetActive(false);
+        }
+        else
+        {
+            upgradeIcons[3].SetActive(false);
+            checkSigns[3].SetActive(false);
+            crossSigns[3].SetActive(true);
+        }
+        
+        if (_experience >= CalculateCost(_barracksLevel, mult[4]))
+        {
+            upgradeIcons[4].SetActive(true);
+            checkSigns[4].SetActive(true);
+            crossSigns[4].SetActive(false);
+        }
+        else
+        {
+            upgradeIcons[4].SetActive(false);
+            checkSigns[4].SetActive(false);
+            crossSigns[4].SetActive(true);
+        }
+        */
+        /*
+        if (_experience > CalculateCost(0))
+            checkSigns[0].SetActive(true);
+        else
+            checkSigns[0].SetActive(false);
+
+        if (_experience > CalculateCost(1))
+            checkSigns[1].SetActive(true);
+        else
+            checkSigns[1].SetActive(false);
+        
+        if (_experience > CalculateCost(2))
+            checkSigns[2].SetActive(true);
+        else
+            checkSigns[2].SetActive(false);
+        
+        if (_experience > CalculateCost(3))
+            checkSigns[3].SetActive(true);
+        else
+            checkSigns[3].SetActive(false);
+        
+        if (_experience > CalculateCost(4))
+            checkSigns[4].SetActive(true);
+        else
+            checkSigns[4].SetActive(false);
+        */
+    }
+
+    public int CalculateCost(int structureLevel, float multiplier)
     {
         int val = (structureLevel + 1) * 5;
 
-        return Mathf.FloorToInt(Mathf.Pow(val, 2.22f)) + 1;
+        return (Mathf.FloorToInt(Mathf.Pow(val, 2.22f) * multiplier) + 1);
     }
 
     public void Upgrade(int structure)
@@ -149,99 +297,103 @@ public class PirateCoveController : MonoBehaviour
         switch (structure)
         {
             case 0:
-                if (RemoveExperience(CalculateCost(_wallLevel)))
+                if (RemoveExperience(CalculateCost(buildingsLevels[0], mult[0])))
                 {
                     //wallModels[_wallLevel].SetActive(false);
                     
-                    _wallLevel += 1;
+                    buildingsLevels[0] += 1;
 
-                    wallModels[_wallLevel].SetActive(true);
-                    GameManager.instance.healthMultiplier = wallUpgrades[_wallLevel];
+                    wallModels[buildingsLevels[0]].SetActive(true);
+                    GameManager.instance.healthMultiplier = wallUpgrades[buildingsLevels[0]];
+
+                    costs[0].text = "" + CalculateCost(buildingsLevels[0], mult[0]);
                 }
                 else
-                    Debug.Log("Insufficient _experience points to purchase Wall Level " + _wallLevel + 1);
+                    Debug.Log("Insufficient _experience points to purchase Wall Level " + buildingsLevels[0] + 1);
 
                 break;
 
             case 1:
-                if (RemoveExperience(CalculateCost(_chestLevel)))
+                if (RemoveExperience(CalculateCost(buildingsLevels[1], mult[1])))
                 {
                     //chestModels[_chestLevel].SetActive(false);
                     
-                    _chestLevel += 1;
+                    buildingsLevels[1] += 1;
 
-                    chestModels[_chestLevel].SetActive(true);
-                    GameManager.instance.moneyMultiplier = chestUpgrades[_chestLevel];
+                    chestModels[buildingsLevels[1]].SetActive(true);
+                    GameManager.instance.moneyMultiplier = chestUpgrades[buildingsLevels[1]];
+                    
+                    costs[1].text = "" + CalculateCost(buildingsLevels[1], mult[1]);
                 }
                 else
-                    Debug.Log("Insufficient _experience points to purchase Wall Level " + _chestLevel + 1);
+                    Debug.Log("Insufficient _experience points to purchase Wall Level " + buildingsLevels[1] + 1);
 
                 break;
 
             case 2:
-                if (RemoveExperience(CalculateCost(_tavernLevel)))
+                if (RemoveExperience(CalculateCost(buildingsLevels[2], mult[2])))
                 {
                     //tavernModels[_tavernLevel].SetActive(false);
                     
-                    _tavernLevel += 1;
+                    buildingsLevels[2] += 1;
 
-                    tavernModels[_tavernLevel].SetActive(true);
-                    GameManager.instance.rangeMultiplier = rangeUpgrades[_tavernLevel];
+                    tavernModels[buildingsLevels[2]].SetActive(true);
+                    GameManager.instance.rangeMultiplier = rangeUpgrades[buildingsLevels[2]];
+                    
+                    costs[2].text = "" + CalculateCost(buildingsLevels[2], mult[2]);
                 }
                 else
-                    Debug.Log("Insufficient _experience points to purchase Wall Level " + _tavernLevel + 1);
+                    Debug.Log("Insufficient _experience points to purchase Wall Level " + buildingsLevels[2] + 1);
 
                 break;
 
             case 3:
-                if (RemoveExperience(CalculateCost(_blacksmithLevel)))
+                if (RemoveExperience(CalculateCost(buildingsLevels[3], mult[3])))
                 {
                     //blacksmithModels[_blacksmithLevel].SetActive(false);
                     
-                    _blacksmithLevel += 1;
+                    buildingsLevels[3] += 1;
                     
-                    blacksmithModels[_blacksmithLevel].SetActive(true);
-                    GameManager.instance.damageMultiplier = towerUpgrades[_blacksmithLevel];
+                    blacksmithModels[buildingsLevels[3]].SetActive(true);
+                    GameManager.instance.damageMultiplier = towerUpgrades[buildingsLevels[3]];
+                    
+                    costs[3].text = "" + CalculateCost(buildingsLevels[3], mult[3]);
                 }
                 else
-                    Debug.Log("Insufficient _experience points to purchase Wall Level " + _blacksmithLevel + 1);
+                    Debug.Log("Insufficient _experience points to purchase Wall Level " + buildingsLevels[3] + 1);
 
                 break;
 
             case 4:
-                if (RemoveExperience(CalculateCost(_barracksLevel)))
+                if (RemoveExperience(CalculateCost(buildingsLevels[4], mult[4])))
                 {
                     //barracksModels[_barracksLevel].SetActive(false);
                     
-                    _barracksLevel += 1;
+                    buildingsLevels[4] += 1;
 
-                    barracksModels[_barracksLevel].SetActive(true);
-                    GameManager.instance.attSpeedMultiplier = 1f / towerUpgrades[_barracksLevel];
+                    barracksModels[buildingsLevels[4]].SetActive(true);
+                    GameManager.instance.attSpeedMultiplier = 1f / towerUpgrades[buildingsLevels[4]];
+                    
+                    costs[4].text = "" + CalculateCost(buildingsLevels[4], mult[4]);
                 }
                 else
-                    Debug.Log("Insufficient _experience points to purchase Wall Level " + _barracksLevel + 1);
-                break;
-
-            case 5:
-                if (RemoveExperience(CalculateCost(_guardTowerLevel)))
-                {
-                    //guardTowerModels[_guardTowerLevel].SetActive(false);
-                    
-                    _guardTowerLevel += 1;
-                    guardTowerModels[_guardTowerLevel].SetActive(true);
-                    //Turret.instance.SetLevel(_guardTowerLevel);
-                }
-                    
-                else
-                    Debug.Log("Insufficient _experience points to purchase Wall Level " + _guardTowerLevel + 1);
+                    Debug.Log("Insufficient _experience points to purchase Wall Level " + buildingsLevels[4] + 1);
                 break;
         }
     }
 
+    private void ValueChanged()
+    {
+        experienceText.text = "" + _experience;
+        
+        CalculateAll();
+    }
 
     public void AddExperience(int num = 1)
     {
         _experience += num;
+        
+        ValueChanged();
     }
 
     public bool RemoveExperience(int num)
@@ -249,36 +401,34 @@ public class PirateCoveController : MonoBehaviour
         if (_experience - num < 0) return false;
 
         _experience -= num;
+        
+        ValueChanged();
+        
         return true;
     }
 
     public int GetWallLevel()
     {
-        return _wallLevel;
+        return buildingsLevels[0];
     }
 
     public int GetChestLevel()
     {
-        return _chestLevel;
+        return buildingsLevels[1];
     }
 
     public int GetTavernLevel()
     {
-        return _tavernLevel;
+        return buildingsLevels[2];
     }
 
     public int GetBlacksmithLevel()
     {
-        return _blacksmithLevel;
+        return buildingsLevels[3];
     }
 
     public int GetBarracksLevel()
     {
-        return _barracksLevel;
-    }
-
-    public int GetGuardTowerLevel()
-    {
-        return _guardTowerLevel;
+        return buildingsLevels[4];
     }
 }
